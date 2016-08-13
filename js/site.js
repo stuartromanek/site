5dac9933d4456885a79e643f43c40dd6fac70732
Stuart Romanek <stuart@punkave.com>
2016-08-13 09:54:46 -0400

    more

--- js/site.js
+++ js/site.js
@@ -7,24 +7,29 @@ var vh = $window.height() * 0.8;
 var vw = $window.width() * 0.8;
 
 $draggable.on( 'dragStart', function( event, pointer, moveVector ) {
-	$(this).css('z-index', zIndex);
+	$(this).css('z-index', zIndex).parent().css('z-index', zIndex);
 	zIndex++;
 })
 
-$draggable.on( 'staticClick', function( event, pointer ) {
-	console.log(event);
+$draggable.on( 'staticClick', function( event, pointer, moveVector ) {
+	$(this).css('z-index', zIndex).parent().css('z-index', zIndex);
+	zIndex++;
 })
 
-$slides.each(function(){
+$draggable.on( 'dragEnd', function( event, pointer, moveVector ) {
+	$controls.parent().css('z-index', zIndex + 1);
+	zIndex++;
+})
+
+$slides.filter(':not(.content)').each(function(){
 	var $self = $(this);
-	// $self.
-	$(this, 'li').css({
+	$self.css({
 		top: Math.floor(Math.random() * vh) + 1  + 'px',
 		left: Math.floor(Math.random() * vw) + 1  + 'px'
 	})
 });
 
-render = function() {
+render = function($this) {
 	$slides.removeClass('active');
 	$controls.find('input:checked').each(function(){
 		var project = $(this).attr('data-project');
@@ -34,7 +39,8 @@ render = function() {
 
 
 $controls.find('input').on('change', function() {
-	render();
+	
+	render($(this));
 });
 
 render();
